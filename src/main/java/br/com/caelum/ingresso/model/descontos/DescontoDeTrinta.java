e97ee11f1558af46eb2399bf8138d10a02700129
Cristiano <cristiano.l.grossi@gmail.com>
2017-09-11 18:13:44 -0300

    atualizacao 110917

---
+++ src/main/java/br/com/caelum/ingresso/model/descontos/DescontoDeTrinta.java
@@ -0,0 +1,21 @@
+package br.com.caelum.ingresso.model.descontos;
+
+import java.math.BigDecimal;
+
+public class DescontoDeTrinta implements Desconto {
+
+	private BigDecimal percentualDeDesconto=new BigDecimal("0.3");
+
+	@Override
+	public BigDecimal aplicarDescontoSobre(BigDecimal precoOriginal) {
+		return precoOriginal.subtract(trintaPorCentoSobre(precoOriginal));
+	}
+
+	private BigDecimal trintaPorCentoSobre(BigDecimal precoOriginal) {
+		BigDecimal x=precoOriginal.multiply(percentualDeDesconto);
+		return x;
+	}
+	
+	
+	
+}
